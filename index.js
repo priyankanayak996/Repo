be3a04fc7611f7c33ea3a3ddb360c9a808cc2cd2
Priyanka <priyanka.nayak@external.merckgroup.com>
2020-07-10 11:54:32 +0200

    Commited

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const joi = require('@hapi/joi');
 const api = require('./api');
 
 // set up express app
