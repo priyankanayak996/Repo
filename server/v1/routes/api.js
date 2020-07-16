87d1dad92db7ef44d9a973e14f80c225f7d4e8da
Priyanka <priyanka.nayak@external.merckgroup.com>
2020-07-16 11:39:34 +0200

    CRUD operations.

--- server/v1/routes/api.js
+++ server/v1/routes/api.js
@@ -1,33 +1,45 @@
 const express = require('express');
-// const jwt = require('jsonwebtoken');
-const router = express.Router();
-const User = require('../model/User');
-const { addUserValidation } = require('../validation/user.validation');
-const Laboratory = require('./laboratory');
 
+const router = express.Router();
+
+const User = require('../model/User');
+
+const {
+  addUserValidation,
+} = require('../validation/user.validation');
+
+const Laboratory = require('./laboratory');
 
 router.get('/user', (req, res) => {
   res.send({
+
     type: 'GET',
+
   });
 });
 
 router.post('/user', addUserValidation, (req, res) => {
   User.create(req.body).then((user) => {
     res.send(user);
+
     /* console.log(req.body);
+
    console.log(req.headers);
 
    res.send({
 
     body:req.body,
+
     headers:req.headers */
   });
 });
+
 // Get
+
 router.get('/laboratory', async (req, res) => {
   try {
     const labs = await Laboratory.find();
+
     res.json(labs);
   } catch (err) {
     res.send(err);
@@ -36,29 +48,38 @@ router.get('/laboratory', async (req, res) => {
 
 // Post
 
-router.post('/laboratory', async (req, res) =>
+router.post('/laboratory', async (req, res) => {
   const lab = new Laboratory({
+
     first_name: req.body.first_name,
+
     last_name: req.body.last_name,
+
     id: req.body.id,
+
     location: req.body.location,
 
   });
 
-try {
-  const saveLab = await lab.save();
-  res.json(saveLab);
-} catch (err) {
-  res.send(err);
-}
-// const token = jwt.sign(_id: lab._id, process.env.TOKEN_SECRET); res.header('auth-token', token).send(token);
+  try {
+    const saveLab = await lab.save();
+
+    res.json(saveLab);
+  } catch (err) {
+    res.send(err);
+  }
 });
+
 // Put
+
 router.put('/laboratory/:id', async (req, res) => {
   try {
     const updateLab = await Laboratory.findByIdAndDelete({
+
       _id: req.params.id,
+
     });
+
     res.json(updateLab);
   } catch (err) {
     res.json(err);
@@ -66,11 +87,15 @@ router.put('/laboratory/:id', async (req, res) => {
 });
 
 // Delete
+
 router.delete('/laboratory/:id', async (req, res) => {
   try {
     const removedLab = await Laboratory.remove({
+
       _id: req.params.id,
+
     });
+
     res.json(removedLab);
   } catch (err) {
     res.json(err);
@@ -78,15 +103,23 @@ router.delete('/laboratory/:id', async (req, res) => {
 });
 
 // patch
+
 router.patch('/laboratory/:id', async (req, res) => {
   try {
     const updatedLab = await Laboratory.updateOne({
+
       _id: req.params.id,
+
     }, {
+
       $set: {
+
         first_name: req.body.first_name,
+
       },
+
     });
+
     res.json(updatedLab);
   } catch (err) {
     res.json(err);
