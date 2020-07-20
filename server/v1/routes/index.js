1fc6fbf7d8e01941f5ed76cad7604bc7c2b541df
Priyanka <priyanka.nayak@external.merckgroup.com>
2020-07-20 18:17:51 +0200

    Changes related to mongoose connection

--- server/v1/routes/index.js
+++ server/v1/routes/index.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+
 const api = require('./api');
 
 // set up express app
 const app = express();
 
-mongoose.connect('mongodb://localhost/user');
-mongoose.Promise = global.Promise;
-
 app.use(bodyParser.json());
 
-app.use('/api', api);
-
-// listen for request
-app.listen(process.env.port || 4000, () => {
-  console.log('now listening for request');
-});
+app.use('/v1/api', api);
